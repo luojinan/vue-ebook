dbc6793ed10723b91eb005c97a60fd48bf78d9da
LuoJinAn <1017386624@qq.com>
2019-10-22 08:39:42 +0800

    创建阅读器路由结构

---
+++ src/components/ebook/EbookReader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ebook-reader">
+    {{$route.params.filename}}
+    <p>图书组件</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ebook-reader{
+  p{
+    color: green;
+  }
+}
+</style>
---
+++ src/views/reader/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ebook">
+    电子书阅读器页面
+    <p>测试scss</p>
+    <ebook-reader></ebook-reader>
+  </div>
+</template>
+
+<script>
+import EbookReader from '@/components/ebook/EbookReader'
+
+export default {
+  components:{
+    EbookReader
+  }  
+}
+</script>
+<style lang="scss" scoped>
+.ebook {
+  p{
+    color: red;
+  }
+}
+</style>
