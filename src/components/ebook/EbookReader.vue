8f536d6cfff9308e26ba686c1e14b8f2d7d1dea9
LuoJinAn <1017386624@qq.com>
2019-10-22 10:24:28 +0800

    完成ngnix静态服务器搭建，使用路由的文件名进行获取文件。处理eslint导致console.log不可用问题

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -1,23 +1,40 @@
 <template>
   <div class="ebook-reader">
-    {{$route.params.filename}}
-    <p>图书组件</p>
+    <div id="reader"></div>
   </div>
 </template>
 
 <script>
+import Epub from 'epubjs'
 export default {
-  data(){
+  data() {
     return {
-
+      rendition:''
     }
+  },
+  methods: {
+    initEpub(bookName) {
+      const url = `http://localhost:8084/${bookName}.epub`
+      const book = new Epub(url)
+      this.rendition = book.renderTo('reader', {
+        width: innerWidth,
+        height: innerHeight
+      })
+      // 把生成的dom渲染进页面
+      this.rendition.display()
+    }
+  },
+  mounted() {
+    const bookName = this.$route.params.filename.split('|').join('/')
+    console.log(bookName)
+    this.initEpub(bookName)
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.ebook-reader{
-  p{
+.ebook-reader {
+  p {
     color: green;
   }
 }
