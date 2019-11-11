56f7e05aeea2f7cf287813f7d9d01b25d3f008c8
LuoJinAn <1017386624@qq.com>
2019-11-11 22:18:27 +0800

    主题颜色设置，改造成国际化

--- src/lang/index.js
+++ src/lang/index.js
@@ -4,13 +4,20 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 // json转js对象
 function loadLocaleMessages () {
+  // 👇返回一个：所有文件在一个函数里(是个require函数)
+  // 后面可以跟require.context(文件夹,正则)(文件)得到里面的值json被转为js对象？
   const locales = require.context('./', true, /[A-Za-z0-9-_,\s]+\.json$/i)
   const messages = {}
+  // 函数的keys()返回文件名数组
+  console.log('文件名数组',locales.keys())  
   locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+    console.log('key',key)  // './cn.json'
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i)  // match匹配出xx,会分割成数组
+    console.log('fileName Array',matched) // ['cn','cn']
     if (matched && matched.length > 1) {
-      const locale = matched[1]
+      const locale = matched[1] // 文件名 'cn'
       messages[locale] = locales(key)
+      console.log('locales函数(传入路径key)',locales(key))
     }
   })
   return messages
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,32 +1,50 @@
-export function themeList(vue){
-  return [
+export function themeList(vue) {
+  return [{
+      name: '默认', // 用于传入epubjs切换主题（只传中文）
+      desc: vue.$t('book.themeDefault'), // 用于国际化显示主题名（中英文等）
+      style: {
+        body: {
+          color: '#000',
+          background: '#fff'
+        }
+      }
+    },
     {
-      name:'默认',  // 用于传入epubjs切换主题（只传中文）
-      desc: vue.$t('book.themeDefault'),  // 用于国际化显示主题名（中英文等）
-      style:{
-      body:{color:'#000',background:'#fff'}
-    }},
-    {
-      name:'护眼',
+      name: '护眼',
       desc: vue.$t('book.themeEye'),
-      style:{
-      body:{color:'#000',background:'#ceeaba'}
-    }},
+      style: {
+        body: {
+          color: '#000',
+          background: '#ceeaba'
+        }
+      }
+    },
     {
-      name:'夜间',
+      name: '夜间',
       desc: vue.$t('book.themeNight'),
-      style:{
-      body:{color:'#fff',background:'#000'}
-    }},
+      style: {
+        body: {
+          color: '#fff',
+          background: '#000'
+        }
+      }
+    },
     {
-      name:'金黄',
+      name: '金黄',
       desc: vue.$t('book.themeGold'),
-      style:{
-      body:{color:'#000',background:'#FFD700'}
-    }}
+      style: {
+        body: {
+          color: '#000',
+          background: '#FFD700'
+        }
+      }
+    }
   ]
 }
 
-export const fontFamilyList=['Default','Cabin','Days One','Montserrat','Tangerine']
+export const fontFamilyList = ['Default', 'Cabin', 'Days One', 'Montserrat', 'Tangerine']
 
-export default {themeList, fontFamilyList}
+export default {
+  themeList,
+  fontFamilyList
+}
