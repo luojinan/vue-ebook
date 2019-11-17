369f2353c59fc13bc5ae889fadfb6ce4f3289ea3
LuoJinAn <1017386624@qq.com>
2019-11-17 11:57:17 +0800

    完成loading组件外壳，待添加蒙板动画实现动态效果

--- src/components/ebook/EbookLoading/index.vue
+++ src/components/ebook/EbookLoading/index.vue
@@ -1,19 +1,54 @@
 <template>
   <div class="ebook-loading">
-    加载中
+    <div class="ebook-loading_wrapp">
+      <div class="ebook-loading_left-right" v-for="(innerList,index1) in lines" :key="index1">
+        <div class="ebook-loading_item" v-for="(item,index) in innerList" :key="index"></div>
+      </div>
+      <div class="ebook-loading_center"></div>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  data(){
+  data() {
     return {
-
+      lines: [[1, 1, 1], [2, 2, 2]]
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-.ebook-loading{
-
+@import "../../../scss/global.scss";
+.ebook-loading {
+  position: relative;
+  width: px2rem(63);
+  height: px2rem(40);
+  border: px2rem(1.5) solid #d7d7d7;
+  border-radius: px2rem(3);
+  .ebook-loading_wrapp{
+    display: flex;
+    height: 100%;
+    width: 100%;
+    .ebook-loading_left-right{
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      padding: 0 px2rem(7);
+      .ebook-loading_item{
+        height: px2rem(2);
+        width: px2rem(18);
+        background-color: #d7d7d7;
+      }
+    }
+    .ebook-loading_center{
+      position: absolute;
+      left: 50%;  //左右居中
+      top: 0;
+      height: 100%;
+      width: px2rem(1.5);
+      background-color: #d7d7d7;
+    }
+  }
 }
 </style>
--- src/components/ebook/EbookSlide/index.vue
+++ src/components/ebook/EbookSlide/index.vue
@@ -2,9 +2,9 @@
 <div class="ebook-slide">
   <transition name="slide-right">
     <div class="toc-content">
-      <!-- <p v-if="!tocAvailable">加载中...</p> -->
+      <!-- <p v-if="!bookAvailable">加载中...</p> -->
       <!-- 动态组件 -->
-      <component v-if="bookAvailable" :is="currentTab===1?toc:bookmark"></component>
+      <component v-if="false" :is="currentTab===1?toc:bookmark"></component>
       <!-- 目录加载中状态 -->
       <ebook-loading v-else></ebook-loading>
       
